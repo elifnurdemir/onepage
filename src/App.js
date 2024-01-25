b3739eb28bb8a97b051c048bfd37c4deb2c62be5
absurdisticca <elifxnur118@gmail.com>
2024-01-25 22:10:12 +0300

    o la la
    Co-authored-by: Mert Metin <necatimertmetin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+import perfume from './assets/media/perfume.png';
+import menu from './assets/media/menu.png'; 
+import rose1 from './assets/media/rose1.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='navbar'>
+        <div className='navbar-left'>
+          Arrivederciao
+        </div>
+        <div className='navbar-right'>
+          <img className='menu-icon' src={menu}/>
+        </div>
+
+      </div>
+      <img src={rose1} className='rose1'/>
+      <div className='content-container'>
+
+        <img className='perfume-img' src={perfume} />
+        <div className='content-wrapper'>Pinky</div>
+        <div className='content-wrapper content-title'>Pinkish</div>
+        <div className='content-wrapper'>Gourmand</div>
+
+      </div>
+
+      <div className='bottom-right-container'>
+      
+        <div className='bottom-right-wrapper'>
+          ethereal
+        </div>
+        <div className='bottom-right-wrapper'>
+         luscious
+        </div>
+        <div className='bottom-right-wrapper'>
+          creamy
+        </div>
+      </div>
+
     </div>
   );
 }
